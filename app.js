aaef8aa03a434f1b0b0eb3c1dbe754cc6d26a8c4
Srijit <srijitdas4321@gmail.com>
2025-06-20 16:57:23 +0530

    Home page add and ui and ux

--- app.js
+++ app.js
@@ -23,6 +23,10 @@ app.get(['/', '/login'], (req, res) => {
     res.sendFile(path.join(__dirname, 'views/auth/login.html'));
 });
 
+app.get('/home', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/home.html'));
+});
+
 app.get('/register', (req, res) => {
     res.sendFile(path.join(__dirname, 'views/auth/register.html'));
 });
